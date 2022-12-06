8154a73da30b89559514ef79a1cf19ec6d6cb064
WuChenDi <wuchendi96@gmail.com>
2022-12-06 15:34:37 +0800

    feat: add users module, swagger and mongoose

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('nestjs-examples')
+    .setDescription('The nestjs-examples API description')
+    .setVersion('1.0')
+    .addTag('nestjs-examples')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, document)
+
+  await app.listen(3000, '0.0.0.0', async () => {
+    console.log(`Application is running on: ${await app.getUrl()}`)
+  })
 }
-bootstrap();
+
+bootstrap()
