cfd7008ea8b646e109342d1579387d78eee59c28
WuChenDi <wuchendi96@gmail.com>
2022-12-06 15:39:06 +0800

    feat: Modify main document methods

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,28 @@
+import { INestApplication } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-
+const setupSwagger = (app: INestApplication) => {
   const config = new DocumentBuilder()
+    .addBearerAuth()
     .setTitle('nestjs-examples')
     .setDescription('The nestjs-examples API description')
     .setVersion('1.0')
     .addTag('nestjs-examples')
     .build()
   const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('docs', app, document)
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true
+    }
+  })
+}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+
+  setupSwagger(app)
 
   await app.listen(3000, '0.0.0.0', async () => {
     console.log(`Application is running on: ${await app.getUrl()}`)
